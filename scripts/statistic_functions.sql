6bf8dfb3e312ce54d3322d0ef7ca5bb5fc7f43ae
konstantin.ovsyannikov <st107930@gmail.com>
2024-05-15 18:14:36 +0300

    wip: added stored procedures and etc

--- scripts/statistic_functions.sql
+++ scripts/statistic_functions.sql
@@ -1,12 +1,12 @@
 /* Скрипты подсчёта статистических величин */
 CREATE OR REPLACE FUNCTION dealer_revenue(dealer_id int, date_part int, mode text) RETURNS int
+
 AS $$
 BEGIN
 IF mode != 'month' AND mode != 'year' THEN
 	RAISE EXCEPTION 'Incorrect date mode!';
 END IF;
-RETURN (select sum(price) FROM (deal JOIN vehicle ON deal.vehicle_fkey = vehicle.id) where dealer_fkey = dealer_id
-AND DATE_PART(mode, date::date) = date_part);
+RETURN (select sum(price) FROM (deal JOIN vehicle ON deal.vehicle_fkey = vehicle.id) WHERE dealer_id = dealer_fkey AND DATE_PART(mode, date::date) = date_part); 
 END
 $$ LANGUAGE plpgsql;
 
@@ -20,7 +20,7 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION monthly_dealer_revenue(dealer_id int, month int) RETURNS int 
 AS $$
 BEGIN
-RETURN dealer_revenue(dealer_id, year, 'month');
+RETURN dealer_revenue(dealer_id, month, 'month');
 END
 $$ LANGUAGE plpgsql;
 
@@ -31,15 +31,18 @@ IF mode != 'month' AND mode != 'year' THEN
 	RAISE EXCEPTION 'Incorrect date mode!';
 END IF;
 
-CREATE TEMP TABLE vehicle_quotient AS SELECT (vehicle_fkey, service_quotient) FROM
+/* временная таблица, хранящая сервисные коэффициенты для каждого ТС */
+CREATE TEMP TABLE vehicle_quotient AS SELECT vehicle_fkey, service_quotient FROM
 ((repair JOIN vehicle ON vehicle.id = vehicle_fkey) JOIN model on model.id = model_fkey);
-CREATE TEMP TABLE vehicle_service AS SELECT (mechanic_fkey, service_fkey) FROM
+
+/* таблица для получения сервиса, в котором производилось обслуживание */
+CREATE TEMP TABLE vehicle_service AS SELECT mechanic_fkey, service_fkey FROM
 (repair JOIN mechanic ON mechanic.id = mechanic_fkey);
 
 RETURN (select sum(operation.base_price * vehicle_quotient.service_quotient) FROM 
 (((repair JOIN vehicle_quotient ON repair.vehicle_fkey = vehicle_quotient.vehicle_fkey) JOIN vehicle_service 
-ON repair.mechanic_fkey = vehicle_service.mehcanic_fkey) JOIN operation ON operation.id = operation_fkey)
-WHERE service_fkey = service_id AND DATE_PART('year', date::date) = year);
+ON repair.mechanic_fkey = vehicle_service.mechanic_fkey) JOIN operation ON operation.id = operation_fkey)
+WHERE service_fkey = service_id AND DATE_PART(mode, date::date) = date_part);
 END
 $$ LANGUAGE plpgsql;
 
@@ -53,6 +56,6 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION monthly_service_revenue(service_id int, month int) RETURNS int
 AS $$
 BEGIN
-RETURN service_revenue(service_id, year, 'month');
+RETURN service_revenue(service_id, month, 'month');
 END
 $$ LANGUAGE plpgsql;
